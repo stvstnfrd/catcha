ea815ecdc14e6560634e6f9044f7e58b5e076f8a
stv <stv@stanford.edu>
2015-02-21 16:31:36 -0800

    WIP

--- install.sh
+++ install.sh
@@ -10,35 +10,38 @@ MYSQL_USER_NAME=catch
 MYSQL_USER_PASS=edx
 MYSQL_TABLE_NAME=catch
 MYSQL_SERVER_HOST=localhost
+SUDO=sudo
 
-sudo apt-get update
+${SUDO} apt-get update
 
 # configure mysql
-sudo debconf-set-selections \
+${SUDO} debconf-set-selections \
     <<< "mysql-server mysql-server/root_password password ${MYSQL_ROOT_PASS}"
-sudo debconf-set-selections \
+${SUDO} debconf-set-selections \
     <<< "mysql-server mysql-server/root_password_again password ${MYSQL_ROOT_PASS}"
-sudo apt-get install mysql-server --yes
+${SUDO} apt-get install mysql-server --yes
 mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASS}" -e \
     "create database catch default charset utf8;"
 mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASS}" -e \
     "grant all on \'${MYSQL_TABLE_NAME}\'.* to \'${MYSQL_USER_NAME}\'@\'${MYSQL_SERVER_HOST}\' identified by \'${MYSQL_USER_PASS}\';"
 
 # should we be installing 1.7?
-sudo apt-get install default-jre --yes
+${SUDO} apt-get install default-jre --yes
 echo "export JAVA_HOME=${JAVA_INSTALL_PATH}" >> "${HOME}/.bashrc"
 export JAVA_HOME=${JAVA_INSTALL_PATH}
 
-sudo apt-get install unzip --yes
-sudo apt-get install curl --yes
+${SUDO} apt-get install unzip --yes
+${SUDO} apt-get install curl --yes
 curl --output "${GVM_INSTALL_SCRIPT}" get.gvmtool.net
 bash "${GVM_INSTALL_SCRIPT}"
-sudo rm "${GVM_INSTALL_SCRIPT}"
+${SUDO} rm "${GVM_INSTALL_SCRIPT}"
 . "${GVM_INIT_SCRIPT}"
 
 # say yes to set as default
 gvm install grails "${GRAILS_VERSION}"
 
+git clone https://github.com/annotationsatharvard/catcha.git /var/catcha
+cd /var/catcha
 cd Catch
 grails run-app
 
